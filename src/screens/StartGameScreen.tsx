c3a608037dc3e0f56bf6045151bf639d1bfe8c56
anna_matviichuk <matviychuk.anna324@gmail.com>
2024-09-05 15:35:36 +0300

    fix: create component files

---
+++ src/screens/StartGameScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export function StartGameScreen({navigation}: any) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>StartGame Screen</Text>
+    </View>
+  );
+}
