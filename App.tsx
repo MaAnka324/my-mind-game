05633913d5f9ff94821a693df0f1958e742c6c6e
anna_matviichuk <matviychuk.anna324@gmail.com>
2024-09-05 15:37:15 +0300

    style: add expo-linear-gradient

--- App.tsx
+++ App.tsx
@@ -2,28 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-function HomeScreen({navigation}: any) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <TouchableOpacity onPress={()=>navigation.navigate('StartGame')}>
-        <Text>
-          Start Game
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
-
-function StartGameScreen({navigation}: any) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>StartGame Screen</Text>
-    </View>
-  );
-}
+import {HomeScreen} from './src/screens/HomeScreen'
+import {StartGameScreen} from './src/screens/StartGameScreen'
 
 const Stack = createNativeStackNavigator();
 
@@ -31,18 +11,24 @@ export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="StartGame" component={StartGameScreen} />
+      <Stack.Screen 
+      name="Home" 
+      component={HomeScreen} 
+      options={{ headerShown: false }}/>
+      <Stack.Screen 
+      name="StartGame" 
+      component={StartGameScreen}
+      options={{ headerShown: false }}/>
     </Stack.Navigator>
   </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
